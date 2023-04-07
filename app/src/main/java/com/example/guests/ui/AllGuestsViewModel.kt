c747b55401b0b5ed3acd9b893284f2bca15e4c85
Vitor Henrique <vitor.henrique@xds.com.br>
2023-04-07 16:00:12 -0300

    classes renomeadas e separação de responsabilidades na MainActivity

--- app/src/main/java/com/example/guests/ui/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/guests/ui/AllGuestsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.guests.ui.slideshow
+package com.example.guests.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AllGuestsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
---
+++ app/src/main/java/com/example/guests/ui/GuestFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guests.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.guests.R
+
+class GuestFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_form)
+    }
+}
