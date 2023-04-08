b166e355bbaf8b7363e1fd9fdf70bc50876421e9
Vitor Henrique <vitor.henrique@xds.com.br>
2023-04-08 17:41:30 -0300

    Configuração da GuestFormActivity e criação da ViewModel

--- app/src/main/java/com/example/guests/ui/GuestFormActivity.kt
+++ app/src/main/java/com/example/guests/ui/GuestFormActivity.kt
@@ -2,11 +2,33 @@ package com.example.guests.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.example.guests.R
+import com.example.guests.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+        configureView()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+        }
+    }
+
+    private fun configureViewModel() {
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+    }
+
+    private fun configureView() {
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
     }
 }
